3b900301456b7053bc3e6870011f2890618a5cff
Charlotte Caors <charlotte.caors@gmail.com>
2020-08-11 12:24:04 +0200

    models done

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :bookings, dependent: :destroy
+  has_many :screenings, dependent: :destroy
+  has_many :reviews, through: :bookings
+  has_many :favorites, dependent: :destroy
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :description, presence: true
+  validates :phone_number, format: { with: /(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})/ }
+end
